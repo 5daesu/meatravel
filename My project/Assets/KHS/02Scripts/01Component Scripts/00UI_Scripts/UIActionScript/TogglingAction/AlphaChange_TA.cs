423815a0ca86cdfe3254b0ce51d2a55db94b2d75
5daesu <100927715+5daesu@users.noreply.github.com>
2024-05-11 18:39:34 +0900

    On fixing github error
    On fixing github error

--- My project/Assets/KHS/02Scripts/01Component Scripts/00UI_Scripts/UIActionScript/TogglingAction/AlphaChange_TA.cs
+++ My project/Assets/KHS/02Scripts/01Component Scripts/00UI_Scripts/UIActionScript/TogglingAction/AlphaChange_TA.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatingToggleWindow : TogglingWindow
+public class AlphaChange_TA : MonoBehaviour
 {
-
+    // Start is called before the first frame update
     void Start()
     {
         
--- My project/Assets/KHS/02Scripts/01Component Scripts/00UI_Scripts/UIActionScript/TogglingAction/BaseClass/TogglingAction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class TogglingAction : MonoBehaviour
-{
-    public abstract void OpenAction();
-    public abstract void CloseAction();
-}
